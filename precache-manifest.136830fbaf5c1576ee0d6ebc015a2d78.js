5f793e4cd62ad4c6cdd16fe4f6bf6446dc31685e
MinaRamsisASD1993 <minaramsis1993@outlook.com>
2018-12-03 14:16:54 +0200

    Updates

---
+++ precache-manifest.136830fbaf5c1576ee0d6ebc015a2d78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65dd574dc71532402377",
+    "url": "/Contact-Manager/static/js/runtime~main.65dd574d.js"
+  },
+  {
+    "revision": "578144e070fee9a83ff1",
+    "url": "/Contact-Manager/static/js/main.578144e0.chunk.js"
+  },
+  {
+    "revision": "c10ca3a9ef1a11855528",
+    "url": "/Contact-Manager/static/js/1.c10ca3a9.chunk.js"
+  },
+  {
+    "revision": "578144e070fee9a83ff1",
+    "url": "/Contact-Manager/static/css/main.c88b1a92.chunk.css"
+  },
+  {
+    "revision": "c10ca3a9ef1a11855528",
+    "url": "/Contact-Manager/static/css/1.133ce568.chunk.css"
+  },
+  {
+    "revision": "de66ff7da61e3b97ba93f7dea08f6c3f",
+    "url": "/Contact-Manager/index.html"
+  }
+];
